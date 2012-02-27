cd32a9fe003b494aaeece898661f078822be1059
Priit Kallas <kallaspriit@gmail.com>
2012-02-27 12:59:57 +0200

    Updated the logo.

--- application/layouts/default.php
+++ application/layouts/default.php
@@ -5,7 +5,7 @@
 	<title>Lightspeed-PHP</title>
 </head>
 <body>
-	<img src="/images/logo.png" width="220" height="38" alt="Lightspeed-PHP" title="Lightspeed-PHP"/>
+	<img src="/images/logo.png" width="220" height="36" alt="Lightspeed-PHP" title="Lightspeed-PHP"/>
 	<?php echo $this->content ?>
 </body>
 </html>
